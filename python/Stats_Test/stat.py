b88fca24cf37fc5fe0d1b924899e562ebcfd69dc
Embee <embee@Embees-MBP.fios-router.home>
2021-05-12 12:02:47 -0400

    more code

---
+++ python/Stats_Test/stat.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+
+list = []
+
+def Add_Num(x : float, n : int) -> int:
+    for i in range(n):
+        list.append(x)
+    
+    return n
+
+
+Add_Num(1.2,3)
+Add_Num(1.32,10)
+Add_Num(1.234543,71)
+
+Np_Array = np.array(list)
+
+
+print("Hello")
+
+print('Hello World')
