d212893b6dd519e0d9466a19149754a7c5951bb5
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-15 13:36:33 -0400

    did some fixes

--- CPP/Stats/stddev.cpp
+++ CPP/Stats/stddev.cpp
@@ -13,13 +13,26 @@ float stddev(vector<double> input)
     }
     t = t/input.size();
     
-    t = (input[0]-t)/input.size();
+    t = ((input[0]-t)*(input[0]-t))/input.size();
    return sqrt(abs(t));
+}
 
+
+float variance(vector<double> input)
+{
+    double t = 0;
+    for(auto x: input)
+    {
+        t+=x;
+    }
+    t = t/input.size();
+    
+    t = ((input[0]-t)*(input[0]-t))/input.size();
+   return abs(t);
 }
 
 int main()
 {
-    vector<double> t{1,2,3,4,5,6,5,4,3,2,4};
-    std::cout << stddev(t) << std::endl;
+    vector<double> t{1,2,3,4,5,6,5,4,3,2,4,2,3,4,5,1,2,3,4,5};
+    std::cout << stddev(t) << std::endl << variance(t) << std::endl;
 }
