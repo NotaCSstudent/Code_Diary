67de811b2562fc830cff762e97434fcc532c4910
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-18 15:31:08 -0400

    parallel

--- CPP/Paralell/parallel.cpp
+++ CPP/Paralell/parallel.cpp
@@ -1,6 +1,8 @@
 
 
 int A[1000];
+int B[1000];
+int C[1000];
 void test() {
 #pragma loop(hint_parallel(0))
     for (int i=0; i<1000; ++i) {
@@ -14,7 +16,35 @@ void test() {
 
 
 
+void loop_test_2(int u) {
+#pragma loop(hint_parallel(8))
+   for (int i=0; i<u; ++i){
+      A[i] = B[i] * C[i];
+   }
+}
+
+
+
+
+
+
+void loop_test(int u)
+ {
+   for (int i=0; i<u; ++i){
+      A[i] = B[i] * C[i];
+}
+ }
+
+
+
+
 int main()
 {
+#pragma loop(no_vector)
+for (int i = 0; i < 1000; ++i)
+{
+   A[i] = B[i] + C[i];
+}
+
     test();
 }
