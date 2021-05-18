15d45b656e87cd7ad6484cc45b6be3e003b1ef5a
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-18 15:25:31 -0400

    stuff

---
+++ CPP/Paralell/parallel.cpp
@@ -0,0 +1,20 @@
+
+
+int A[1000];
+void test() {
+#pragma loop(hint_parallel(0))
+    for (int i=0; i<1000; ++i) {
+        A[i] = A[i] + 1;
+    }
+
+    for (int i=1000; i<2000; ++i) {
+        A[i] = A[i] + 1;
+    }
+}
+
+
+
+int main()
+{
+    test();
+}
