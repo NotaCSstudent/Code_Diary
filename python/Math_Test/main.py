e8e67c927705aedc19243551fd21cadd035c0c66
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-22 09:33:37 -0400

    more py stuff

---
+++ python/Math_Test/main.py
@@ -0,0 +1,15 @@
+class Math_Stuff:
+    def __init__(self,x:int,y:int,z:int):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.t = self.x+self.y+self.z
+    def add():
+        self.t = self.x + self.y + self.z
+    def multiply():
+        self.t = self.x * self.y * self.z
+
+
+t = Math_Stuff(1,2,3)
+t.add()
+t.multiply()
