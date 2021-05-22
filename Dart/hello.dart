7271e4c36abf6de1d5b17303ee709e0ffc6b688b
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-22 10:14:38 -0400

    dart

--- Dart/hello.dart
+++ Dart/hello.dart
@@ -1,4 +1,16 @@
+class Test
+{
+  int x = 0;
+  int y = 0;
+  int z = 0;
+  Test(int a,int b, int c)
+  {
+    x = a;
+    y = b;
+    z = c;
+  }
 
+}
 
 int hello()
 {
@@ -10,4 +22,6 @@ void main()
   print('Hello Dart');
   hello();
   print(hello());
+  var first = Test(1,2,3);
+  print(first.x);
 }
