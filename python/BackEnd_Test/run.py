7f3a3f59ddbc9f31eb8bab914a889c0faf02c8cd
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-23 16:36:42 -0400

    hello

---
+++ python/BackEnd_Test/run.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+'''
+for i in range(0,10):
+    print(np.sin(i))
+'''
+
+class Obj_:
+    def __init__(self):
+        self.x = x
+    def __add__(self,second):
+        self.x += second.x
+    def __sub__(self,second):
+        self.x -= second.x
+    def __mul__(self,second):
+        self.x *= second.x
+
+
